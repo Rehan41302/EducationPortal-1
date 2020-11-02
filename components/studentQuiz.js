6776bb7d719680d4798676698874e1698e8db714
muhammad asrlan <muhammad.arslan@denovers.com>
2020-11-02 23:26:47 +0500

    bfre mrg

--- components/studentQuiz.js
+++ components/studentQuiz.js
@@ -2,10 +2,11 @@ import React from 'react';
 // import { Text, View,Linking, StyleSheet, Button,ScrollView,TextInput,TouchableOpacity, } from 'react-native';
 import * as FileSystem from 'expo-file-system'
 import { Avatar, Button, Card, Title, Paragraph,} from "react-native-paper";
-import {View,Text,FlatList, StyleSheet, Alert,ScrollView} from 'react-native'
+import {View,Text,FlatList, StyleSheet, Alert,ScrollView, Linking} from 'react-native'
 import { connect } from "react-redux";
 import firebase from "../config/fire";
-
+import PDFReader from 'rn-pdf-reader-js'
+// import {FileSystem} from 'expo-file-system'
 class StudentQuiz extends React.Component{
  
   renderItem = ({ item, index }) => {
@@ -15,23 +16,39 @@ console.log(item)
       <Card style={styles.card}>
         <Card.Content>
           <Title>Teacher : {item.name} </Title>
-          <Title>Subject : {item.subject} </Title>
+          <Title>Subject : {item.subject}  </Title>
 
             {item?.quiz&& item.quiz.map((quiz,index)=>{
+              console.log(quiz.pdfLink)
+              let co = 'https://nextjs.org/docs/deployment'
               return (
-                <Title>Quiz no {index+1} :  
-                  <Button onPress={               
-              ()=>this.downloadQuiz(item)}> Download </Button>
-                 </Title>
-              )
+                <Button onPress={
+                  ()=> {
+                 
+                      FileSystem.downloadAsync(
+                       'http://gahp.net/wp-content/uploads/2017/09/sample.pdf',
+                       FileSystem.documentDirectory + 'small.pdf'
+                     )
+                       .then(({ uri }) => {
+                         console.log('Finished downloading to ', uri);
+                       })
+                       .catch(error => {
+                         console.error(error);
+                       });
+                 
+                                       
+                  } }>Download </Button>              )
             })}
           {/* <Paragraph>Time & Date</Paragraph> */}
         </Card.Content>
         {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
         <Card.Actions>
+           
           {item?.quiz&&
           <Button onPress={
-             ()=>this.enroll(item)}>Upload </Button>
+             ()=> {
+               Linking.openURL('http://gahp.net/wp-content/uploads/2017/09/sample.pdf')
+             } }>Upload </Button>
           }
         </Card.Actions>
       </Card>
@@ -41,7 +58,12 @@ console.log(item)
       console.log(this.props.tutors?.data,'tutorsol')
    return(
     <ScrollView>
-    {/* <Text>HElo</Text> */}
+    <Text>HElo</Text>
+    <PDFReader
+        source={{
+          uri: 'http://gahp.net/wp-content/uploads/2017/09/sample.pdf',
+        }}
+      />
   {this.props.tutors?.data.length>0?
   <FlatList
     data={this.props.tutors?.data}
