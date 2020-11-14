9857927a9fb31d91c8c1b4c019890b7a387ab0d6
Rehan Saeed <43314251+Rehan41302@users.noreply.github.com>
2020-11-14 14:27:29 +0500

    Update fire.js

--- config/fire.js
+++ config/fire.js
@@ -1,20 +1,13 @@
 import * as firebase from "firebase";
 
 var firebaseConfig = {
-  // apiKey: "AIzaSyCdpjIVF8eghoybcVrd-R2xrIIwxrTT87s",
-  // authDomain: "tutorapp-1fb1f.firebaseapp.com",
-  // databaseURL: "https://tutorapp-1fb1f.firebaseio.com",
-  // projectId: "tutorapp-1fb1f",
-  // storageBucket: "tutorapp-1fb1f.appspot.com",
-  // messagingSenderId: "815400909950",
-  // appId: "1:815400909950:web:6c66b52c9f6f025308ac2f"
-  apiKey: "AIzaSyDQesIJzYaCFrJ1B4wi0lSOUEhPHSjpSWk",
-  authDomain: "educationporal.firebaseapp.com",
-  databaseURL: "https://educationporal.firebaseio.com",
-  projectId: "educationporal",
-  storageBucket: "educationporal.appspot.com",
-  messagingSenderId: "857575476920",
-  appId: "1:857575476920:web:7f6876799a7790cc1eae4d"
+  apiKey: "AIzaSyCdpjIVF8eghoybcVrd-R2xrIIwxrTT87s",
+  authDomain: "tutorapp-1fb1f.firebaseapp.com",
+  databaseURL: "https://tutorapp-1fb1f.firebaseio.com",
+  projectId: "tutorapp-1fb1f",
+  storageBucket: "tutorapp-1fb1f.appspot.com",
+  messagingSenderId: "815400909950",
+  appId: "1:815400909950:web:6c66b52c9f6f025308ac2f"
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
